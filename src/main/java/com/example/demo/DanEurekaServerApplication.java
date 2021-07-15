cecd12f67f9fe279f6459a66bde7fb49d2e8d0d9
Tomas Ravelli <tomy_ravelli@hotmail.com>
2021-07-15 18:59:37 -0300

    Configuracion eureka server

--- src/main/java/com/example/demo/DanEurekaServerApplication.java
+++ src/main/java/com/example/demo/DanEurekaServerApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import jdk.jfr.Enabled;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DanEurekaServerApplication {
 
 	public static void main(String[] args) {
